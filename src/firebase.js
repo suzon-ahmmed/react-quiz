af0f956438d4c641bef4729618b52e547ac86fac
suzon-ahmmed <info.suzonahamed@gmail.com>
2022-09-24 20:36:12 +0600

    netlify deploy testing

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,31 @@
+// import { initializeApp } from "firebase/app";
+
+// // firebase configuration
+// const app = initializeApp({
+//   apiKey: process.env.REACT_APP_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET_ID,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_ID,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+// });
+
+// export default app;
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// firebase configuration
-const app = initializeApp({
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET_ID,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-});
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export default app;
